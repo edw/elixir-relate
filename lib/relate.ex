a54d5b50911e07e3dd2d386058cc9c6ca8df5a6e
Edwin Watkeys <edw@poseur.com>
2019-04-12 13:05:39 -0400

    Streamline arg handling, and tests and doctests

--- lib/relate.ex
+++ lib/relate.ex
@@ -1,29 +1,49 @@
 defmodule Relate do
   @moduledoc """
-  Relate implements relational operators on Elixir enumerables. It
-  currently supports four types of joins: inner, left, right, and
-  outer.
+
+  Relate implements relational operators on Elixir enumerables.
+
+  Functions take two enumerables containing objects as well as one or
+  two arguments to specify how the value to use as a basis for joining
+  will be determined.
+
+  If either `fki1` or `fki2` are functions, the function will be
+  called with the object as an argument on `e1` or `e2`,
+  respectively. If the value is an atom, the object will be treated as
+  a map and the join value will be the value of the key associated
+  with the atom. If the value is a non-negative integer, the object
+  will be treated as a tuple and the value will be treated as an index
+  into it.
+
+  If `fki2` is not specified or is `nil` or `false`, the same accessor
+  will be used on both `e1` and `e2`.
+
   """
 
-  def inner_join(e1, e2, f_or_k), do: inner_join(e1, e2, f_or_k, f_or_k)
+  @doc ~S"""
 
-  def inner_join(e1, e2, k1, k2) when is_atom(k1) and is_atom(k2) do
-    inner_join(e1, e2, & &1[k1], & &1[k2])
-  end
+  Return an enumerable of tuples containing all elements of `e1` and
+  `e2` which yield the same value when `fki1` and `fki2` are applied,
+  respectively. If `fki2` is `nil` or `false`, `fki1` will be used to
+  make comparisons on both enumerables.
 
-  def inner_join(e1, e2, k1, k2) when is_integer(k1) and is_atom(k2) do
-    inner_join(e1, e2, &elem(&1, k1), & &1[k2])
-  end
+  ### Examples
 
-  def inner_join(e1, e2, k1, k2) when is_atom(k1) and is_integer(k2) do
-    inner_join(e1, e2, & &1[k1], &elem(&1, k2))
-  end
+    iex> Relate.inner_join([%{k: 0, v: "zero"}, %{k: 1, v: "one"}],
+    ...>                   [%{k: 1, v: "i"}, %{k: 2, v: "ii"}],
+    ...>                   :k, :k)
+    [{%{k: 1, v: "one"}, %{k: 1, v: "i"}}]
 
-  def inner_join(e1, e2, k1, k2) when is_integer(k1) and is_integer(k2) do
-    inner_join(e1, e2, &elem(&1, k1), &elem(&1, k2))
-  end
+    iex> Relate.inner_join([%{k: 0, v: "zero"}, %{k: 1, v: "one"}],
+    ...>                   [%{k: 1, v: "i"}, %{k: 2, v: "ii"}],
+    ...>                   :k)  # NOTE: only one key function
+    [{%{k: 1, v: "one"}, %{k: 1, v: "i"}}]
 
-  def inner_join(e1, e2, f1, f2) do
+  """
+  def inner_join(e1, e2, fki1, fki2 \\ nil) do
+    f1 = to_f(fki1)
+    f2 = to_f(fki2 || f1)
+
     for x1 <- e1,
         x2 <- e2,
         f1.(x1) == f2.(x2) do
@@ -31,25 +51,32 @@ defmodule Relate do
     end
   end
 
-  def left_join(e1, e2, f_or_k), do: left_join(e1, e2, f_or_k, f_or_k)
+  @doc """
 
-  def left_join(e1, e2, k1, k2) when is_atom(k1) and is_atom(k2) do
-    left_join(e1, e2, & &1[k1], & &1[k2])
-  end
+  Return an enumerable of tuples containing all elements of `e1` and
+  `e2` which yield the same value when `fki1` and `fki2` are applied,
+  respectively. If `fki2` is `nil` or `false`, `fki1` will be used to
+  make comparisons on both enumerables. Additionally, a tuple of the
+  form `{i, nil}` will be returned for every element of `e1` that did
+  not match on any element in `e2`.
 
-  def left_join(e1, e2, k1, k2) when is_integer(k1) and is_atom(k2) do
-    left_join(e1, e2, &elem(&1, k1), & &1[k2])
-  end
+  ### Examples
 
-  def left_join(e1, e2, k1, k2) when is_atom(k1) and is_integer(k2) do
-    left_join(e1, e2, & &1[k1], &elem(&1, k2))
-  end
+    iex> Relate.left_join([%{k: 0, v: "zero"}, %{k: 1, v: "one"}],
+    ...>                  [%{k: 1, v: "i"}, %{k: 2, v: "ii"}],
+    ...>                  :k, :k)
+    [{%{k: 0, v: "zero"}, nil}, {%{k: 1, v: "one"}, %{k: 1, v: "i"}}]
 
-  def left_join(e1, e2, k1, k2) when is_integer(k1) and is_integer(k2) do
-    left_join(e1, e2, &elem(&1, k1), &elem(&1, k2))
-  end
+    iex> Relate.left_join([%{k: 0, v: "zero"}, %{k: 1, v: "one"}],
+    ...>                  [%{k: 1, v: "i"}, %{k: 2, v: "ii"}],
+    ...>                  :k)  # NOTE: only one key function
+    [{%{k: 0, v: "zero"}, nil}, {%{k: 1, v: "one"}, %{k: 1, v: "i"}}]
 
-  def left_join(e1, e2, f1, f2) do
+  """
+  def left_join(e1, e2, fki1, fki2 \\ nil) do
+    f1 = to_f(fki1)
+    f2 = to_f(fki2 || f1)
+
     for x <- e1, reduce: [] do
       acc ->
         Enum.concat(acc, left_join_helper(x, e2, f1, f2))
@@ -70,25 +97,32 @@ defmodule Relate do
     )
   end
 
-  def right_join(e1, e2, f_or_k), do: right_join(e1, e2, f_or_k, f_or_k)
+  @doc """
 
-  def right_join(e1, e2, k1, k2) when is_atom(k1) and is_atom(k2) do
-    right_join(e1, e2, & &1[k1], & &1[k2])
-  end
+  Return an enumerable of tuples containing all elements of `e1` and
+  `e2` which yield the same value when `fki1` and `fki2` are applied,
+  respectively. If `fki2` is `nil` or `false`, `fki1` will be used to
+  make comparisons on both enumerables. Additionally, a tuple of the
+  form `{nil, i}` will be returned for every element of `e2` that did
+  not match on any element in `e1`.
 
-  def right_join(e1, e2, k1, k2) when is_integer(k1) and is_atom(k2) do
-    right_join(e1, e2, &elem(&1, k1), & &1[k2])
-  end
+  ### Examples
 
-  def right_join(e1, e2, k1, k2) when is_atom(k1) and is_integer(k2) do
-    right_join(e1, e2, & &1[k1], &elem(&1, k2))
-  end
+    iex> Relate.right_join([%{k: 0, v: "zero"}, %{k: 1, v: "one"}],
+    ...>                   [%{k: 1, v: "i"}, %{k: 2, v: "ii"}],
+    ...>                   :k, :k)
+    [{%{k: 1, v: "one"}, %{k: 1, v: "i"}}, {nil, %{k: 2, v: "ii"}}]
 
-  def right_join(e1, e2, k1, k2) when is_integer(k1) and is_integer(k2) do
-    right_join(e1, e2, &elem(&1, k1), &elem(&1, k2))
-  end
+    iex> Relate.right_join([%{k: 0, v: "zero"}, %{k: 1, v: "one"}],
+    ...>                   [%{k: 1, v: "i"}, %{k: 2, v: "ii"}],
+    ...>                   :k)  # NOTE: only one key function
+    [{%{k: 1, v: "one"}, %{k: 1, v: "i"}}, {nil, %{k: 2, v: "ii"}}]
 
-  def right_join(e1, e2, f1, f2) do
+  """
+  def right_join(e1, e2, fki1, fki2 \\ nil) do
+    f1 = to_f(fki1)
+    f2 = to_f(fki2 || f1)
+
     for x <- e2, reduce: [] do
       acc ->
         Enum.concat(acc, right_join_helper(x, e1, f1, f2))
@@ -109,26 +143,34 @@ defmodule Relate do
     )
   end
 
-  def outer_join(e1, e2, f_or_k), do: outer_join(e1, e2, f_or_k, f_or_k)
+  @doc """
 
-  def outer_join(e1, e2, k1, k2) when is_atom(k1) and is_atom(k2) do
-    outer_join(e1, e2, & &1[k1], & &1[k2])
-  end
+  Return an enumerable of tuples containing all elements of `e1` and
+  `e2` which yield the same value when `fki1` and `fki2` are applied,
+  respectively. If `fki2` is `nil` or `false`, `fki1` will be used to
+  make comparisons on both enumerables. Additionally, a tuple of the
+  form `{nil, i}` or `{i, nil}` will be returned for every element of
+  `e2` that did not match on any element in `e1` and vice-versa.
 
-  def outer_join(e1, e2, k1, k2) when is_integer(k1) and is_atom(k2) do
-    outer_join(e1, e2, &elem(&1, k1), & &1[k2])
-  end
+  ### Examples
 
-  def outer_join(e1, e2, k1, k2) when is_atom(k1) and is_integer(k2) do
-    outer_join(e1, e2, & &1[k1], &elem(&1, k2))
-  end
+    iex> Relate.outer_join([%{k: 0, v: "zero"}, %{k: 1, v: "one"}],
+    ...>                   [%{k: 1, v: "i"}, %{k: 2, v: "ii"}],
+    ...>                   :k, :k)
+    [{%{k: 0, v: "zero"}, nil}, {%{k: 1, v: "one"}, %{k: 1, v: "i"}}, {nil, %{k: 2, v: "ii"}}]
 
-  def outer_join(e1, e2, k1, k2) when is_integer(k1) and is_integer(k2) do
-    outer_join(e1, e2, &elem(&1, k1), &elem(&1, k2))
-  end
+    iex> Relate.outer_join([%{k: 0, v: "zero"}, %{k: 1, v: "one"}],
+    ...>                   [%{k: 1, v: "i"}, %{k: 2, v: "ii"}],
+    ...>                   :k)  # NOTE: only one key function
+    [{%{k: 0, v: "zero"}, nil}, {%{k: 1, v: "one"}, %{k: 1, v: "i"}}, {nil, %{k: 2, v: "ii"}}]
 
-  def outer_join(e1, e2, f1, f2) do
-    Enum.concat(left_join(e1, e2, f1, f2), right_join(e1, e2, f1, f2))
+  """
+  def outer_join(e1, e2, fki1, fki2 \\ nil) do
+    Enum.concat(left_join(e1, e2, fki1, fki2), right_join(e1, e2, fki1, fki2))
     |> Enum.uniq()
   end
+
+  defp to_f(f) when is_function(f), do: f
+  defp to_f(k) when is_atom(k), do: & &1[k]
+  defp to_f(i) when is_integer(i) and i >= 0, do: &elem(&1, i)
 end
--- test/relate_test.exs
+++ test/relate_test.exs
@@ -1,4 +1,53 @@
 defmodule RelateTest do
   use ExUnit.Case
   doctest Relate
+
+  setup do
+    {
+      :ok,
+      english: [
+        {1, "one"},
+        {0, "zero"},
+        {2, "two"},
+        {3, "three"},
+        {4, "four"},
+        {5, "five"},
+        {6, "six"},
+        {7, "seven"}
+      ],
+      roman: [
+        %{n: 1, str: "i"},
+        %{n: 2, str: "ii"},
+        %{n: 3, str: "iii"},
+        %{n: 4, str: "iiii"},
+        %{n: 4, str: "iv"},
+        %{n: 5, str: "v"},
+        %{n: 6, str: "vi"},
+        %{n: 7, str: "vii"},
+        %{n: 8, str: "viii"},
+        %{n: 9, str: "viiii"},
+        %{n: 9, str: "ix"},
+        %{n: 10, str: "x"}
+      ]
+    }
+  end
+
+  test "outer_join works against a tuple and map enums",
+       %{english: english, roman: roman} do
+    assert Relate.outer_join(english, roman, 0, :n) |> Enum.sort() == [
+             {nil, %{n: 8, str: "viii"}},
+             {nil, %{n: 9, str: "ix"}},
+             {nil, %{n: 9, str: "viiii"}},
+             {nil, %{n: 10, str: "x"}},
+             {{0, "zero"}, nil},
+             {{1, "one"}, %{n: 1, str: "i"}},
+             {{2, "two"}, %{n: 2, str: "ii"}},
+             {{3, "three"}, %{n: 3, str: "iii"}},
+             {{4, "four"}, %{n: 4, str: "iiii"}},
+             {{4, "four"}, %{n: 4, str: "iv"}},
+             {{5, "five"}, %{n: 5, str: "v"}},
+             {{6, "six"}, %{n: 6, str: "vi"}},
+             {{7, "seven"}, %{n: 7, str: "vii"}}
+           ]
+  end
 end
